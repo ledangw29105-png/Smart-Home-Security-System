2f050bba2087dbbeb65c5ef65815fe5b976975e9
ledangw29105-png <ledangw29105@gmail.com>
2026-06-18 07:48:41 +0700

    Add files via upload

---
+++ esp32cam/ESP32-Cam-Đã thêm OTA/esp32cam.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <Arduino.h>
+#include "esp_camera.h"
+
+void prepareForDeepSleep();
+void initHardware();
+bool initCamera();
+bool connectWiFi();
+bool connectMQTT();
+void reconnectMQTT();
+
+bool detectMotion();
+camera_fb_t *captureImage();
+bool sendImage(camera_fb_t *frameBuffer);
+void publishMotionStatus(bool status);
+
+void turnOnBuzzer();
+void turnOffBuzzer();
+void turnOnLED();
+void turnOffLED();
+
+void receiveCommand(char *topic, byte *payload, unsigned int length);
+void executeCommand(const String &command);
+
+void startCameraWebServer();
---
+++ esp32cam/ESP32-Cam-Đã thêm OTA/ota_update.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <Arduino.h>
+#include <stdint.h>
+
+class PubSubClient;
+
+extern const char *OTA_COMMAND_TOPIC;
+extern const char *OTA_STATUS_TOPIC;
+
+typedef void (*OtaTaskCallback)();
+
+void otaInit(PubSubClient *mqttClient);
+void otaSetTaskControlCallbacks(
+  OtaTaskCallback onPause,
+  OtaTaskCallback onResume
+);
+void otaHandleMqttMessage(
+  const char *topic,
+  const uint8_t *payload,
+  unsigned int length
+);
+bool performOTA(
+  const String &fileUrl,
+  const String &expectedMd5 = ""
+);
